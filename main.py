e05d732a2f4546dcf7a7dba368a2a9d53844f148
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-05-29 18:26:44 +1000

    Initial file content
    main.py setup
    basic template fro quiz and reader

---
+++ main.py
@@ -0,0 +1,8 @@
+from quiz import Quiz
+
+def main():
+    quiz = Quiz()
+    quiz.run()
+
+if __name__ == "__main__":
+    main()
---
+++ quiz.py
@@ -0,0 +1,5 @@
+from reader import Reader
+
+class Quiz:
+    def __init__(self):
+        pass
